93f069440ef4e87a10970c8ee4fc77e2eb3a6427
samuelguar <43284050+PapoGato@users.noreply.github.com>
2025-03-07 23:48:34 -0500

    added basics in app (html, js, css)

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-let URL = "https://pokeapi.co/api/v2/" ;
+const listaPokemon = document.querySelector("#listaPokemon");
+let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i=0;i<152;i++){
-    fetch (URL + i)
-        .then((response)=>response.json())
-        .then (data => mostrarPokemon(data))
-}
+const obtenerPokemon = async () => {
+    for (let i = 1; i <= 1025; i++) {
+        const response = await fetch(URL + i);
+        const data = await response.json();
+        mostrarPokemon(data);
+    }
+};
 
-console.log("Hola")
+const mostrarPokemon = (data) => {
+    let tipos = data.types.map(type => `
+        <p class="${type.type.name} type">${type.type.name}</p>
+    `).join('');
+
+    const div = document.createElement("div");
+    div.classList.add("pokemon");
+    div.innerHTML = `
+        <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}" />
+        <span class="info-container">
+          <h2>${data.name}</h2>
+          <h3>#${data.id}</h3>
+        </span>
+        <div class="types">
+        ${tipos}
+        </div>
+        <div class="pokemon-stats">
+          <p class="stat" style="margin-bottom: 0;">${data.height} M</p>
+          <p class="stat">${data.weight} KG</p>
+        </div>
+    `;
+    listaPokemon.append(div);
+};
+
+obtenerPokemon();
